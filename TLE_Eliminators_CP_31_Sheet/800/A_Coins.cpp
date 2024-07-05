357605399e686b84cf870d7165e6e23f664093d3
mahabub5090 <mdmahabub.rahman789@gmail.com>
2024-07-05 23:07:40 +0600

    22th_to_26th_800_Rated_problem_solutions_from_TLE_Eliminators_CP_31_Sheet

---
+++ TLE_Eliminators_CP_31_Sheet/800/A_Coins.cpp
@@ -0,0 +1,49 @@
+// Problem Link: https://codeforces.com/problemset/problem/1814/A
+
+/*
+**************************************************************
+*Bis'Millah Hir Rah'Manir Rahim == start with the name of \n *
+*                    ALLAH Subhana Tayala                    *
+*                Author: Md. Mahabubar Rahman                *
+*               CSE,RPI && Rangpur, Bangladesh               *
+**************************************************************
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define Allhamdulillah 0
+#define pb(x) push_back(x)
+#define pp() pop_back()
+#define ll long long int
+#define foi(i, s, e) for (int i = s; i < e; i++)
+#define cn(x) cin >> x
+#define endl '\n'
+#define Endl '\n'
+#define nl cout << '\n'
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+void solve()
+{
+    ll n,k;cin>>n>>k;
+    for(int i=0;i<2;i++){
+        if(n-(i*k)>=0 && (n-(i*k))%2==0){yes;return;}
+    }
+    no;
+}
+/*mdmahabub55*/
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    // t = 1;
+
+    while (t--)
+    {
+        solve();
+    }
+    return Allhamdulillah;
+}
